fcece45f979a5debe39d04d0bffb1a7d0c57ca90
Rmpx <horaciofernandezgar@gmail.com>
2022-06-14 14:19:54 -0500

    Header modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,46 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const NavBar = styled.nav`
-    align-items: center;
-    background-color: white;
-    box-shadow: 2px 2px 5px #999;
-    display: flex;
-    flex-flow: row wrap;
-    margin: 20px;
-    max-height: 125px;
-    padding: 15px;
-    border-radius: 5px;
+  align-items: center;
+  background-color: white;
+  box-shadow: 2px 2px 5px #999;
+  display: flex;
+  /* flex-flow: row wrap; */
+  margin: 20px;
+  max-height: 125px;
+  padding: 15px;
+  border-radius: 5px;
 
-    .cart-logo{
-        size: 50px;
-    }
-    .logo{
-        padding: 5px;
-        cursor: pointer;
-    }
-
-    .brand-text{
-        font-family: 'century gothic';
-        padding-left: 3px;
-        padding-right: 5px;
-        font-weight: bold;
-        color: black;
-        align-content: center;
-        text-decoration: none;
+    .leftSideHeader{
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        width: 50%;
     }
 
-    input{
-       width: 20%;
-       height: 30px;       
+  .rightSideHeader {
+      display: flex;
+      right: 0;
+      margin-left: auto;
+      width: 50%;
+      gap: 10px;
+      align-items: center;
+
+      input {
+        width: 100%;
+        max-height: 25px;
+        padding: 8px;
+        border-radius: 5px;
+        border-width: 0.5px;
+      
     }
-    
+
+    img {
+      max-width: 50px;
+    }
+  }
+
+  
 `;
 
 export default NavBar;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import NavBar from './Header';
-import smLogo from '../assets/logo-50.png';
-import smCart from '../assets/cart-64.png';
+import React from "react";
+import NavBar from "./Header";
+import smLogo from "../assets/logo-50.png";
+import smCart from "../assets/cart-64.png";
 
-export const Header = ({componentOn}) => {
-
-  
-
+export const Header = ({ componentOn }) => {
   return (
-   
-     
-      <NavBar>
-      
-      <img onClick={() => componentOn('home')} className='logo' src={smLogo} alt="logo" />    
-      <h3 className='brand-text'>Luxury Furniture</h3>             
-        <input 
-          type="text" 
-          className="search-input" 
-          placeholder='King size bed' 
-          
+    <NavBar>
+      <div className="leftSideHeader">
+        <img
+          onClick={() => componentOn("home")}
+          className="logo"
+          src={smLogo}
+          alt="logo"
         />
-        <a href='/' className='cart-logo'><img src={smCart} alt="cart-logo" /></a>
-      </NavBar>
-    
-  )
-}
+        <h3 className="brand-text">Luxury Furniture</h3>
+      </div>
+      <div className="rightSideHeader">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="King size bed"
+        />
+        <a href="/" className="cart-logo">
+          <img src={smCart} alt="cart-logo" />
+        </a>
+      </div>
+    </NavBar>
+  );
+};
